ed880ae7dbfc0d4e7ac590a30b548c46189962ef
Sahith Akella <sahithsahith84@gmail.com>
2026-08-29 14:14:38 +0530

    define shared types for rate limiter

--- internal/middleware/ratelimit/ratelimit.go
+++ internal/middleware/ratelimit/ratelimit.go
@@ -1,1 +1,21 @@
 package ratelimit
+
+import "time"
+
+type RateLimitConfig struct {
+	MaxRequestsPerMinute int
+	WindowSizeInSeconds  time.Duration
+}
+
+type RateLimitResult struct {
+	Allowed           bool
+	Remaining         int
+	ResetTimestamp    time.Time // Epoch timestamp when the rate limit will reset
+	RetryAfterSeconds int
+}
+
+// RateLimitStrategy defines the interface for rate limiting strategies.
+type RateLimitStrategy interface {
+	// Check checks if a request is allowed based on the rate limit configuration.
+	Check(identifier string, config RateLimitConfig) RateLimitResult
+}

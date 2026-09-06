500367cd33878094182d7c897fdde31bc98a1b5c
sahithakellacodes <sahithsahith84@gmail.com>
2026-09-06 12:57:29 +0530

    added test script

---
+++ scripts/testtokenbucket.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Running rate limiter concurrency tests..."
+
+REDIS_ADDR=127.0.0.1:6379 go test -race -v -run TestRedisTokenBucketStrategy_Concurrent ./internal/middleware/ratelimit/...
